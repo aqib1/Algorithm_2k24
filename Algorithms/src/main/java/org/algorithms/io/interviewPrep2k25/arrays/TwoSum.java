7bec82361c7a51dff2957923c3a830046bdf2f98
aqib javed <aqibbutt3078@gmail.com>
2023-02-07 01:02:03 +0000

    Two sum using extra space

---
+++ Algorithms/src/main/java/org/algorithms/io/interviewPrep2k25/arrays/TwoSum.java
@@ -0,0 +1,19 @@
+package org.algorithms.io.interviewPrep2k25.arrays;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    // Time complexity O(n) and space O(n)
+    public int[] twoSum(int[] nums, int target) {
+        var deltaByIndexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int delta = target - nums[i];
+            if (deltaByIndexMap.containsKey(delta)) {
+                return new int[]{i, deltaByIndexMap.get(delta)};
+            } else {
+                deltaByIndexMap.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
