0b571d2c464864a0b5e9a358cf34a75163bbfbcc
aqib javed <aqibbutt3078@gmail.com>
2023-02-11 01:02:03 +0000

    Two sum

---
+++ Algorithms/src/main/java/org/algorithms/io/wise/TwoSum.java
@@ -0,0 +1,22 @@
+package org.algorithms.io.wise;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    // Time O(n) and space O(n)
+    public int[] twoSum(int[] nums, int target) {
+        var deltaByIndex = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int delta = target - nums[i];
+            if(deltaByIndex.containsKey(delta)) {
+                return new int[] {i, deltaByIndex.get(delta)};
+            }
+
+            deltaByIndex.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
