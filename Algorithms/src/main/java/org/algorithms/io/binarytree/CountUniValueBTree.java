015dd12a90b16373531ef182ce450d136cef1e90
aqib javed <aqibbutt3078@gmail.com>
2024-02-07 01:02:03 +0500

    Count uni value BTree using bottom up approach

---
+++ Algorithms/src/main/java/org/algorithms/io/binarytree/CountUniValueBTree.java
@@ -0,0 +1,29 @@
+package org.algorithms.io.binarytree;
+
+public class CountUniValueBTree {
+    private int count;
+    public int countUnivalSubtrees(TreeNode root) {
+        countUni(root);
+        return count;
+    }
+
+    private boolean countUni(TreeNode root) {
+        if(root == null)
+            return true;
+
+        boolean left = countUni(root.left);
+        boolean right = countUni(root.right);
+
+        if(left && right) {
+            if((root.left != null && root.val != root.left.val)
+                            || (root.right != null && root.val != root.right.val)) {
+                return false;
+            }
+
+            count++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
