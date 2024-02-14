e73e27b9c470130f03f054b58155ee657b11b45a
aqib javed <aqibbutt3078@gmail.com>
2024-02-14 01:02:03 +0000

    Leetcode 51 | N Quees Problem using backtracking

--- Algorithms/src/main/java/org/algorithms/io/backtracking/NQueens.java
+++ Algorithms/src/main/java/org/algorithms/io/backtracking/NQueens.java
@@ -4,68 +4,104 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NQueens {
+
+    // Time complexity O(n!) and Space is O(n^2)
+    private final List<List<String>> response = new ArrayList<>();
+    private char[][] board;
+    private int numberOfQueens;
+
     public static void main(String[] args) {
-        var queens = new NQueens();
-        var response = queens.solveNQueens(4);
-        for(var d: response) {
+        var queen = new NQueens();
+        var res = queen.solveNQueens(4);
+        for (var d : res) {
             System.out.println(d);
         }
     }
-    private List<List<String>> chessBoard;
-    private int numberOfQueens;
 
     public List<List<String>> solveNQueens(int n) {
         this.numberOfQueens = n;
-        this.chessBoard = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            var subChess = new ArrayList<String>();
-            for(int j = 0; j < n; j++) {
-                subChess.add(".");
-            }
-            chessBoard.add(subChess);
-        }
-
-        setQueens(0);
-        return chessBoard;
+        initBoard();
+        setQueens(0, 0, this.numberOfQueens);
+        return response;
     }
 
-    public boolean setQueens(int column) {
-        if(column == numberOfQueens)
-            return true;
-        for(int row = 0; row < numberOfQueens; row++) {
-            if(isValidPlace(row, column)) {
-                chessBoard.get(row).set(column, "Q");
-                if(setQueens(column + 1)) {
-                    return true;
-                }
-                chessBoard.get(row).set(column, ".");
-            }
-
+    private void setQueens(int row, int col, int n) {
+        if(col == this.numberOfQueens) {
+            col = 0;
+            row++;
         }
-        return false;
+
+        if(n == 0) {
+            response.add(createBoard(board));
+            return;
+        }
+
+        if(row == this.numberOfQueens) {
+            return;
+        }
+
+        if(isValidMove(row, col)) {
+            board[row][col] = 'Q';
+            n--;
+            setQueens(row, col + 1, n);
+            n++;
+            board[row][col] = '.';
+        }
+
+        setQueens(row, col + 1, n);
     }
 
-    private boolean isValidPlace(int row, int column) {
-        for(int c = column; c>=0; c--) {
-            if("Q".equals(chessBoard.get(row).get(c))) {
+    private boolean isValidMove(int row, int col) {
+        // top-down and left-right check
+        for(int i = 0; i < numberOfQueens; i++) {
+            if(board[i][col] == 'Q') {
+                return false;
+            }
+
+            if(board[row][i] == 'Q') {
                 return false;
             }
         }
 
-        // diagonal to top left
-        for(int r = row, c = column; r >= 0 && c >= 0; r--, c--) {
-            if("Q".equals(chessBoard.get(r).get(c))) {
+        // check diagonal from top left to current and current to bottom right
+        for(int r=row, c=col; c>=0 && r>=0; c--, r--) {
+            if(board[r][c] == 'Q')
                 return false;
-            }
+        }
+        for(int r=row, c=col; c<numberOfQueens && r<numberOfQueens; c++, r++) {
+            if(board[r][c] == 'Q')
+                return false;
         }
 
-        // diagonal to bottom right
-        for(int r = row, c = column; r < numberOfQueens && c >= 0; r++, c--) {
-            if("Q".equals(chessBoard.get(r).get(c))) {
+        // check diagonal from current to bottom left and current to top right
+        for(int r=row, c=col; r<numberOfQueens && c>=0; r++, c--) {
+            if(board[r][c] == 'Q')
                 return false;
-            }
+        }
+
+        for(int r=row, c=col; r>=0 && c<numberOfQueens; r--, c++) {
+            if(board[r][c] == 'Q')
+                return false;
         }
 
         return true;
     }
+
+    private List<String> createBoard(char[][] board) {
+        var res = new ArrayList<String>();
+        for(var data : board) {
+            res.add(new String(data));
+        }
+        return res;
+    }
+
+    private void initBoard() {
+        this.board = new char[numberOfQueens][numberOfQueens];
+        for(int i = 0; i < numberOfQueens; i++) {
+            for(int j = 0; j < numberOfQueens; j++) {
+                this.board[i][j] = '.';
+            }
+        }
+    }
+
 }
