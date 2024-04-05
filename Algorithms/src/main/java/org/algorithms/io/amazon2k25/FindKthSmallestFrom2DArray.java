123bf008834b0a645cc94e1b7b499619966102df
aqib javed <aqibbutt3078@gmail.com>
2024-04-05 01:02:03 +0100

    Leetcode 378 | Find kth smallest from 2d Array using binary search

--- Algorithms/src/main/java/org/algorithms/io/amazon2k25/FindKthSmallestFrom2DArray.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k25/FindKthSmallestFrom2DArray.java
@@ -7,7 +7,7 @@ public class FindKthSmallestFrom2DArray {
     public static void main(String[] args) {
         var f = new FindKthSmallestFrom2DArray();
         System.out.println(
-                f.kthSmallestUsingHeap(
+                f.kthSmallestUsingBinarySearch(
                         new int[][] {
                                 {1,5,9},
                                 {10,11,13},
@@ -32,5 +32,42 @@ public class FindKthSmallestFrom2DArray {
         }
 
         return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+
+
+    // Total O(N * Log(max - min)) and space is O(1)
+    public int kthSmallestUsingBinarySearch(int[][] matrix, int k) {
+        int low = matrix[0][0];
+        int high = matrix[matrix.length - 1][matrix.length - 1];
+        // this is OLog(max - min)
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int countLessOrEqual = countLessOrEqual(matrix, mid);
+            if(countLessOrEqual < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    // this is O(N)
+    public int countLessOrEqual(int[][] matrix, int mid) {
+        int column = matrix.length - 1;
+        int row = 0;
+        int count = 0;
+
+        while(column >= 0 && row < matrix.length) {
+            if(matrix[row][column] <= mid) {
+                count += (column + 1);
+                row++;
+            } else {
+                column--;
+            }
+        }
+
+        return count;
     }
 }
