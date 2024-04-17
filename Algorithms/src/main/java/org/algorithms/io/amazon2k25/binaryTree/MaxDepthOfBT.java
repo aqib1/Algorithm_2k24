7795507fd12fffb5bbf85c167274e79d0468b9e7
aqib javed <aqibbutt3078@gmail.com>
2024-04-17 01:02:03 +0100

    Max Depth of a BT using head recursion

--- Algorithms/src/main/java/org/algorithms/io/amazon2k25/binaryTree/MaxDepthOfBT.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k25/binaryTree/MaxDepthOfBT.java
@@ -5,7 +5,10 @@ import org.algorithms.io.tree.TreeNode;
 public class MaxDepthOfBT {
 
     public int maxDepth(TreeNode root) {
-        return maxDepth(root, 0);
+        if(root == null)
+            return 0;
+        maxDepthUsingTailRecursion(root, 1);
+        return max;
     }
 
     // Time complexity O(N) and space O(N)
@@ -17,5 +20,18 @@ public class MaxDepthOfBT {
                 maxDepth(root.left, depth),
                 maxDepth(root.right, depth)
         ) + 1;
+    }
+
+    // Time complexity O(N) and space O(N)
+    int max = Integer.MIN_VALUE;
+    public void maxDepthUsingTailRecursion(TreeNode root, int depth) {
+        if(root == null)
+            return;
+
+        if(root.left == null && root.right == null)
+            max = Math.max(max, depth);
+
+        maxDepthUsingTailRecursion(root.left, depth + 1);
+        maxDepthUsingTailRecursion(root.right, depth + 1);
     }
 }
