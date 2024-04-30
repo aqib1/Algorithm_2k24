c44980fc0461ff3a2acc624d72ca00a135334825
aqib javed <aqibbutt3078@gmail.com>
2024-04-30 01:02:03 +0100

    Leetcode 383 | Can construct

--- Algorithms/src/main/java/org/algorithms/io/amazon2k25/trie/CanConstruct.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k25/trie/CanConstruct.java
@@ -1,30 +1,23 @@
 package org.algorithms.io.amazon2k25.trie;
 
-import java.util.HashMap;
 
 public class CanConstruct {
+    // Time complexity O(n) and space O(26)
     public boolean canConstruct(String ransomNote, String magazine) {
-        var charCount = new HashMap<Character, Integer>();
-        for(var ch: magazine.toCharArray()) {
-            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        var count = new int[26];
+        for(var m: magazine.toCharArray()) {
+            count[m - 'a']++;
         }
-
-        for(var ch: ransomNote.toCharArray()) {
-            if(!charCount.containsKey(ch))
+        for(var r: ransomNote.toCharArray()) {
+            if(--count[r - 'a'] < 0) {
                 return false;
-
-            charCount.put(ch, charCount.get(ch) - 1);
-
-            if(charCount.get(ch) < 0)
-                return false;
+            }
         }
-
         return true;
     }
 
     public static void main(String[] args) {
         var can = new CanConstruct();
-
         System.out.println(can.canConstruct("aa", "aab"));
     }
 }
