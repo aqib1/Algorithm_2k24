957fa6bced5b99e12606f390931889c826a5baf8
aqib javed <aqibbutt3078@gmail.com>
2024-05-30 01:02:03 +0100

    Russian peasant method recursive to multiply two numbers

--- Algorithms/src/main/java/org/algorithms/io/bit/RussianPeasantMultiplication.java
+++ Algorithms/src/main/java/org/algorithms/io/bit/RussianPeasantMultiplication.java
@@ -2,7 +2,7 @@ package org.algorithms.io.bit;
 
 public class RussianPeasantMultiplication {
     public static void main(String[] args) {
-        System.out.println(peasantMultiply(3, 5));
+        System.out.println(peasantMultiplyRecur(4, 2));
     }
 
     // Time complexity OLog(n) and space O(1)
@@ -20,5 +20,25 @@ public class RussianPeasantMultiplication {
         }
 
         return result;
+    }
+
+    public static int peasantMultiplyRecur(int a, int b) {
+        if(b == 0)
+            return 0;
+        else {
+            int result = peasantMultiplyRecur(a << 1, b >> 1);
+            if((b ^ 1) == b - 1)
+                result += a;
+
+            return result;
+        }
+    }
+
+    public static int peasantMultiplyRecur(int a, int b, int result) {
+        if(b == 0) return result;
+
+        if((b ^ 1) == b - 1) result += a;
+
+        return peasantMultiplyRecur(a << 1, b >> 1, result);
     }
 }
