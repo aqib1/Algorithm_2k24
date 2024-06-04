4c9361f9bfe3433b1440e0f5bc43248908ee9d03
aqib javed <aqibbutt3078@gmail.com>
2024-06-04 01:02:03 +0100

    Insertion sort time and space complexity

--- Algorithms/src/main/java/org/algorithms/io/sorting/InsertionSort.java
+++ Algorithms/src/main/java/org/algorithms/io/sorting/InsertionSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 // Time complexity O(n)2
+// Space complexity O(1)
 public class InsertionSort {
     public static void main(String[] args) {
         var insertionSort = new InsertionSort();
@@ -35,6 +36,7 @@ public class InsertionSort {
         }
     }
 
+    // Time complexity O(n)2
     public void reverseInsertionSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             int key = arr[i];
