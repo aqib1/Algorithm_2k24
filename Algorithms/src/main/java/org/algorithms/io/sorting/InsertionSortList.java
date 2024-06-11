c611823ea6351ea193abeb0363192199d467b6fc
aqib javed <aqibbutt3078@gmail.com>
2024-06-11 01:02:03 +0100

    Insertion sort linked list

--- Algorithms/src/main/java/org/algorithms/io/sorting/InsertionSortList.java
+++ Algorithms/src/main/java/org/algorithms/io/sorting/InsertionSortList.java
@@ -2,7 +2,41 @@ package org.algorithms.io.sorting;
 
 import org.algorithms.io.linkedlist.ListNode;
 
+import java.util.ArrayList;
+
 public class InsertionSortList {
+    // Time O(n)2
+    // Space O(n)
+    public ListNode anotherWay(ListNode head) {
+        var data = new ArrayList<Integer>();
+        while(head != null) {
+            data.add(head.val);
+            head = head.next;
+        }
+
+        insertionSort(data);
+
+        var response = new ListNode(0);
+        var ptr = response;
+        for(var d : data) {
+            ptr.next = new ListNode(d);
+            ptr = ptr.next;
+        }
+        return response.next;
+    }
+
+    private void insertionSort(ArrayList<Integer> data) {
+        for(int i = 1; i < data.size(); i++) {
+            int key = data.get(i);
+            int j = i - 1;
+            while(j >= 0 && key <= data.get(j)) {
+                data.set(j + 1, data.get(j));
+                j--;
+            }
+            data.set(j + 1, key);
+        }
+    }
+
     /*
     * Time complexity O(n)2 + Space O(1)
     * */
