eb1f97a5edfd0dac77a7abc16b4732459a9ab83a
aqib javed <aqibbutt3078@gmail.com>
2024-06-27 01:02:03 +0100

    Leetcode 872 | Leaf similar simplified

--- Algorithms/src/main/java/org/algorithms/io/binarytree/IsLeafSimilar.java
+++ Algorithms/src/main/java/org/algorithms/io/binarytree/IsLeafSimilar.java
@@ -1,27 +1,18 @@
 package org.algorithms.io.binarytree;
 
 import java.util.LinkedList;
-import java.util.Objects;
 import java.util.Queue;
 
 public class IsLeafSimilar {
 
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         Queue<Integer> leaf1 = new LinkedList<>();
-        fillUpLeafs(root1, leaf1);
-
         Queue<Integer> leaf2 = new LinkedList<>();
+
+        fillUpLeafs(root1, leaf1);
         fillUpLeafs(root2, leaf2);
 
-        if(leaf1.size() != leaf2.size())
-            return false;
-
-       while(!leaf1.isEmpty()) {
-           if(!Objects.equals(leaf1.poll(), leaf2.poll()))
-               return false;
-       }
-
-        return true;
+        return leaf1.equals(leaf2);
     }
 
 
