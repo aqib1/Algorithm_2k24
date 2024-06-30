05c3fc67684c2445e379397e84d4a4433a67f91e
aqib javed <aqibbutt3078@gmail.com>
2024-06-30 01:02:03 +0100

    Leetcode 222 | Count nodes of Complete tree

--- Algorithms/src/main/java/org/algorithms/io/binarytree/CountNodes.java
+++ Algorithms/src/main/java/org/algorithms/io/binarytree/CountNodes.java
@@ -1,6 +1,8 @@
 package org.algorithms.io.binarytree;
 
 public class CountNodes {
+    // Time complexity O(n)
+    // Space complexity O(n)
     public int countNodes(TreeNode root) {
         return root == null ? 0 : countNodes(root.left) + countNodes(root.right) + 1;
     }
