a74c64257749ac4070cde570c9ce40c885a50566
aqib javed <aqibbutt3078@gmail.com>
2024-07-07 01:13:53 +0100

    Bubble sort in java

---
+++ Algorithms/src/main/java/org/algorithms/io/arrays/BubbleSort.java
@@ -0,0 +1,34 @@
+package org.algorithms.io.arrays;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        System.out.println(
+                Arrays.toString(
+                        bubbleSort(new int[]{
+                                4, 5, 1, 2, 33, 12, 0
+                        }))
+        );
+    }
+
+    private static int[] bubbleSort(int[] data) {
+        int limit = data.length - 1;
+        while(limit >= 0) {
+            for (int i = 0; i < limit; i++) {
+                if (data[i] > data[i + 1]) {
+                    swap(data, i, i + 1);
+                }
+            }
+            limit--;
+        }
+        return data;
+    }
+
+    private static void swap(int[] data, int i, int i1) {
+        int val = data[i];
+        data[i] = data[i1];
+        data[i1] = val;
+    }
+}
