fdfbcb1e67a2f144553827ad109b709481fe0655
aqib javed <aqibbutt3078@gmail.com>
2024-07-13 03:54:12 +0100

    Infix operator converstion to postfix (reverse polish notion) and calculate mathematical operation

--- Algorithms/src/main/java/org/algorithms/io/stacks/InfixToPostfix.java
+++ Algorithms/src/main/java/org/algorithms/io/stacks/InfixToPostfix.java
@@ -4,8 +4,38 @@ import java.util.Stack;
 
 public class InfixToPostfix {
     public static void main(String[] args) {
-        String infix = "A*(B+C)-D/(E+F)";
-        System.out.println(infixToPostfix(infix));
+        String infix = "(1+(4+5+2)-3)+(6+8)";
+        System.out.println(
+                calculatePostfix(
+                        infixToPostfix(infix)
+                )
+        );
+    }
+
+    private static int calculatePostfix(String postfix) {
+        var operandStack = new Stack<Integer>();
+        for (char ch : postfix.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                operandStack.push(Integer.parseInt("" + ch));
+            } else {
+                int second = operandStack.pop();
+                int first = operandStack.pop();
+                int result = operation(first, second, ch);
+                operandStack.push(result);
+            }
+        }
+
+        return operandStack.pop();
+    }
+
+    private static int operation(int first, int second, char ch) {
+        return switch (ch) {
+            case '+' -> first + second;
+            case '-' -> first - second;
+            case '*' -> first * second;
+            case '/' -> first / second;
+            default -> 0;
+        };
     }
 
     private static String infixToPostfix(String infix) {
@@ -13,12 +43,12 @@ public class InfixToPostfix {
         var operatorStack = new Stack<Character>();
 
         for (char ch : infix.toCharArray()) {
-            if (Character.isAlphabetic(ch) || Character.isDigit(ch)) {
+            if (Character.isDigit(ch)) {
                 postfix.append(ch);
             } else {
                 if (!operatorStack.isEmpty()) {
                     var opTop = operatorStack.pop();
-                    if(opTop == '(') {
+                    if (opTop == '(') {
                         operatorStack.push(opTop);
                     } else if (precedence(opTop) > precedence(ch) && precedence(ch) != -1) {
                         postfix.append(opTop);
@@ -26,9 +56,9 @@ public class InfixToPostfix {
                         operatorStack.push(opTop);
                     }
                 }
-                if(ch == ')') {
+                if (ch == ')') {
                     char pop;
-                    while((pop= operatorStack.pop()) != '(') {
+                    while ((pop = operatorStack.pop()) != '(') {
                         postfix.append(pop);
                     }
                 } else {
@@ -37,7 +67,7 @@ public class InfixToPostfix {
             }
         }
 
-        while(!operatorStack.isEmpty()) {
+        while (!operatorStack.isEmpty()) {
             postfix.append(operatorStack.pop());
         }
 
