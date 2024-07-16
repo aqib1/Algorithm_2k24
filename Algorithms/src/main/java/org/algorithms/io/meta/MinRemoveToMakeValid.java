1d47bcfb30234dfe0618bcbcf973782982d28f42
aqib javed <aqibbutt3078@gmail.com>
2024-07-16 01:02:03 +0100

    Leetcode 1249 | Minimum remove to make valid using stack

--- Algorithms/src/main/java/org/algorithms/io/meta/MinRemoveToMakeValid.java
+++ Algorithms/src/main/java/org/algorithms/io/meta/MinRemoveToMakeValid.java
@@ -1,13 +1,41 @@
 package org.algorithms.io.meta;
 
+import java.util.Stack;
+
 public class MinRemoveToMakeValid {
     public static void main(String[] args) {
         var min = new MinRemoveToMakeValid();
         System.out.println(
-                min.minRemoveToMakeValid(
-                        "))(("
+                min.makeRemoveToMakeValidUsingStack(
+                        "a)b(c)d"
                 )
         );
+    }
+
+    // Time complexity O(n)
+    // Space complexity O(n)
+    public String makeRemoveToMakeValidUsingStack(String s) {
+        var response = new StringBuilder();
+        var stack = new Stack<Character>();
+        for(char ch : s.toCharArray()) {
+            if(ch == ')' && stack.isEmpty()) {
+                continue;
+            } else {
+                response.append(ch);
+            }
+
+            if(ch == '(') stack.push(ch);
+            if(ch == ')') stack.pop();
+
+        }
+
+        for(int x=response.length() - 1; x>=0 && !stack.isEmpty(); x--) {
+            if(stack.peek() == response.charAt(x)) {
+                response.deleteCharAt(x);
+                stack.pop();
+            }
+        }
+        return response.toString();
     }
 
     // Time complexity O(n)
