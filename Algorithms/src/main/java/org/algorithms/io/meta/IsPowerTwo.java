c1f562d07b7fb3a9ce36e9774e6fedf6ec4bfbc7
aqib javed <aqibbutt3078@gmail.com>
2024-07-29 01:02:03 +0100

    Leetcode 231 | Power of two time complexity and space complexity

--- Algorithms/src/main/java/org/algorithms/io/meta/IsPowerTwo.java
+++ Algorithms/src/main/java/org/algorithms/io/meta/IsPowerTwo.java
@@ -5,6 +5,9 @@ public class IsPowerTwo {
         var is = new IsPowerTwo();
         System.out.println(is.isPowerOfTwo(14));
     }
+
+    // Time complexity OLong(n)
+    // Space complexity O(1)
     public boolean isPowerOfTwo(int n) {
         if(n == 0)
             return false;
