f44a81904e9512db74905f8e05260a7b30e7bdb7
aqib javed <aqibbutt3078@gmail.com>
2024-08-03 01:02:03 +0100

    Leetcode 231 | Power of two

---
+++ Algorithms/src/main/java/org/algorithms/io/meta/IsPowerTwo.java
@@ -0,0 +1,16 @@
+package org.algorithms.io.meta;
+
+public class IsPowerTwo {
+    public static void main(String[] args) {
+        var is = new IsPowerTwo();
+        System.out.println(is.isPowerOfTwo(14));
+    }
+    public boolean isPowerOfTwo(int n) {
+        if(n == 0)
+            return false;
+
+        while(n % 2 == 0) n /= 2;
+
+        return n == 1;
+    }
+}
