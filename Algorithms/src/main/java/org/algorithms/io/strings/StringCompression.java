903fe125ce802605c4cabb031db6854c909981ac
Aqib Javed <aqibbutt3078@gmail.com>
2024-08-06 17:14:35 +0100

    Update StringCompression.java

--- Algorithms/src/main/java/org/algorithms/io/strings/StringCompression.java
+++ Algorithms/src/main/java/org/algorithms/io/strings/StringCompression.java
@@ -1,46 +1,55 @@
 package org.algorithms.io.strings;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class StringCompression {
 
-    //["a","3","b","2","a","2"]
     public static void main(String[] args) {
         var sc = new StringCompression();
-
+        var str = new char[]{
+                'a','a','a','b','b','a','a'
+        };
         System.out.println(sc.compress(
-                        new char[]{
-                                'a','a','a','b','b','a','a'
-                        }
+                       str
                 )
         );
+
+        System.out.println(Arrays.toString(str));
     }
 
+    // Time complexity O(N) + O(N) = O2(N) = O(N)
+    // Space complexity O(N)
     public int compress(char[] chars) {
         if (chars.length == 1) {
             return 1;
         }
 
         var result = new ArrayList<Character>(chars.length);
-        var charByCount = new HashMap<Character, Integer>();
-        for (char aChar : chars) {
-            charByCount.merge(aChar, 1, Integer::sum);
-        }
-
-        for (char aChar : charByCount.keySet()) {
-            result.add(aChar);
-            var count = charByCount.get(aChar);
-            if(count > 1) {
-                result.addAll(String.valueOf(count)
-                        .chars().mapToObj(i -> (char) i).toList());
+        int count = 1;
+        for (int x = 0; x < chars.length; x++) {
+            if(x == chars.length - 1) {
+                result.add(chars[x]);
+                addCountGreaterThanOne(count, result);
+            } else if(chars[x] == chars[x + 1]) {
+                count++;
+            } else {
+                result.add(chars[x]);
+                addCountGreaterThanOne(count, result);
+                count = 1;
             }
-
         }
 
         for(int i = 0; i < result.size(); i++) {
             chars[i] = result.get(i);
         }
         return result.size();
+    }
+
+    private void addCountGreaterThanOne(int count, ArrayList<Character> result) {
+        if(count > 1) {
+            result.addAll(String.valueOf(count)
+                    .chars().mapToObj(i -> (char) i).toList());
+        }
     }
 }
