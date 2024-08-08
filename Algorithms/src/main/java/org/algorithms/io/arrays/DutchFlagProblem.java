b22d12ebc48528e89172a8bde05a04472b2abb22
Aqib Javed <aqibbutt3078@gmail.com>
2024-08-08 23:53:51 +0100

    Update DutchFlagProblem.java

--- Algorithms/src/main/java/org/algorithms/io/arrays/DutchFlagProblem.java
+++ Algorithms/src/main/java/org/algorithms/io/arrays/DutchFlagProblem.java
@@ -1,1 +1,30 @@
+package org.algorithms.io.arrays;
+
+public class DutchFlagProblem {
+    public void sortColors(int[] nums) {
+        if(nums == null || nums.length == 1) {
+            return;
+        }
+
+        int start = 0, mid = 0, end = nums.length - 1;
+        while(mid <= end) {
+            if(nums[mid] == 0) {
+                swap(nums, start, mid);
+                start++;
+                mid++;
+            } else if(nums[mid] == 2){
+                swap(nums, mid, end);
+                end--;
+            } else {
+                mid++;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
 
