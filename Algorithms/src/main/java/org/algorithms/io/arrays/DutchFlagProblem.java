68479750e6de1c1d725e33ba05824561acef51e3
Aqib Javed <aqibbutt3078@gmail.com>
2024-08-09 00:14:29 +0100

    Update DutchFlagProblem.java

--- Algorithms/src/main/java/org/algorithms/io/arrays/DutchFlagProblem.java
+++ Algorithms/src/main/java/org/algorithms/io/arrays/DutchFlagProblem.java
@@ -1,18 +1,29 @@
 package org.algorithms.io.arrays;
 
-public class DutchFlagProblem {
-    public void sortColors(int[] nums) {
-        if(nums == null || nums.length == 1) {
-            return;
-        }
+import java.util.Arrays;
 
+public class DutchFlagProblem {
+    public static void main(String[] args) {
+        var dutch = new DutchFlagProblem();
+        int[] numbers = new int[]{2, 2, 1, 0, 2, 1, 0, 0, 1};
+        dutch.sortColors(
+                numbers
+        );
+        System.out.println(
+                Arrays.toString(numbers)
+        );
+    }
+
+    // Space complexity O(1)
+    // Time complexity O(N) where N is number of elements
+    public void sortColors(int[] nums) {
         int start = 0, mid = 0, end = nums.length - 1;
-        while(mid <= end) {
-            if(nums[mid] == 0) {
+        while (mid <= end) {
+            if (nums[mid] == 0) {
                 swap(nums, start, mid);
                 start++;
                 mid++;
-            } else if(nums[mid] == 2){
+            } else if (nums[mid] == 2) {
                 swap(nums, mid, end);
                 end--;
             } else {
@@ -21,10 +32,11 @@ public class DutchFlagProblem {
         }
     }
 
-    public void swap(int[] nums, int i, int j) {
+    private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-}
 
+
+}
