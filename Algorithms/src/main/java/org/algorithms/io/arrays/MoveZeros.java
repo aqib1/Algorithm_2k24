52201ff8ea2ad474ddb5eebe0fa3218555352a14
Aqib Javed <aqibbutt3078@gmail.com>
2024-08-10 03:49:07 +0100

    Update MoveZeros.java

--- Algorithms/src/main/java/org/algorithms/io/arrays/MoveZeros.java
+++ Algorithms/src/main/java/org/algorithms/io/arrays/MoveZeros.java
@@ -5,30 +5,27 @@ import java.util.Arrays;
 public class MoveZeros {
     public static void main(String[] args) {
         var moveZeros = new MoveZeros();
-        int[] nums = new int[]{0, 1, 23, 0, 11, 0, 123};
+        int[] nums = new int[]{1, 1, 23, 0, 11, 0, 123};
         moveZeros.moveZeroes(nums);
 
         System.out.println(Arrays.toString(nums));
     }
 
+    // Time complexity O(N)
+    // Space complexity O(1)
     public void moveZeroes(int[] nums) {
         if (nums == null || nums.length == 1)
             return;
 
-        int end = nums.length - 1;
-        while (end >= 0) {
-            for (int j = 0; j < end; j++) {
-                if(nums[j] == 0) {
-                    swap(nums, j, j + 1);
-                }
+        int cursor = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[cursor++] = nums[i];
             }
-            end--;
         }
-    }
 
-    private void swap(int[] nums, int start, int end) {
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
+        while(cursor < nums.length) {
+            nums[cursor++] = 0;
+        }
     }
 }
