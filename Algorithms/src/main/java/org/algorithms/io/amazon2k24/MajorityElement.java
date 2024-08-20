1d4bbddb4989e0d756748280f7437876ef3e2f7e
aqib javed <aqibbutt3078@gmail.com>
2024-08-20 01:02:03 +0100

    Leetcode-169 | Majority Element program

--- Algorithms/src/main/java/org/algorithms/io/amazon2k24/MajorityElement.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k24/MajorityElement.java
@@ -1,7 +1,24 @@
 package org.algorithms.io.amazon2k24;
 
+import java.util.HashMap;
+
 public class MajorityElement {
+    public static void main(String[] args) {
+    }
+
     public int majorityElement(int[] nums) {
+        int moreThan = nums.length / 2;
+        var count = new HashMap<Integer, Integer>();
+        for(int i : nums) {
+            count.put(i, count.getOrDefault(i, 0) + 1);
+        }
+
+        for(var key: count.keySet()) {
+            var val = count.get(key);
+            if(val > moreThan) {
+                return key;
+            }
+        }
 
         return 0;
     }
