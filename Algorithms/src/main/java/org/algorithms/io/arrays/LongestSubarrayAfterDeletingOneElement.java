4cae3205fada8d66b47e95c27cae00cba22739ce
Aqib Javed <aqibbutt3078@gmail.com>
2024-08-25 02:59:42 +0100

    Update LongestSubarrayAfterDeletingOneElement.java

--- Algorithms/src/main/java/org/algorithms/io/arrays/LongestSubarrayAfterDeletingOneElement.java
+++ Algorithms/src/main/java/org/algorithms/io/arrays/LongestSubarrayAfterDeletingOneElement.java
@@ -4,12 +4,37 @@ public class LongestSubarrayAfterDeletingOneElement {
 
     public static void main(String[] args) {
         System.out.println(
-                longestSubarray(
+                longestSubArrayByRemovingOne(
                         new int[]{
                                 1, 1, 0, 1
                         }
                 )
         );
+    }
+
+    // Time complexity O(n*m) and space O(1)
+    public static int longestSubArrayByRemovingOne(int[] nums) {
+        int start = 0, end = 0;
+        int longestSubArray = Integer.MIN_VALUE;
+        int zeroCount = 0;
+
+        while(end < nums.length) {
+            if(nums[end] == 0) {
+                zeroCount++;
+            }
+
+            while(zeroCount > 1) {
+                if(nums[start] == 0) {
+                    zeroCount--;
+                }
+                start++;
+            }
+
+            longestSubArray = Math.max(longestSubArray, end - start);
+            end++;
+        }
+
+        return longestSubArray;
     }
 
     // Time complexity O(n) and space complexity O(1)
