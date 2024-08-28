da26580134299aa71b0a48ebdea0ff4564fdec78
aqib javed <aqibbutt3078@gmail.com>
2024-08-28 01:02:03 +0100

    Leetcode-70 | climb stairs using dynamic tabularization

--- Algorithms/src/main/java/org/algorithms/io/amazon2k24/ClimbStairs.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k24/ClimbStairs.java
@@ -26,4 +26,19 @@ public class ClimbStairs {
         memorization[n] = climbStairsMemorization(n - 1, memorization) + climbStairsMemorization(n - 2, memorization);
         return memorization[n];
     }
+
+    // Using Tabularization dynamic programming.
+    // Time complexity O(n)
+    // Space O(n)
+    public int climbStairsTabularization(int n) {
+        if(n == 1)
+            return 1;
+        var dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <=n ; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
 }
