a7a44fc9b6de5e3c6ea502630c56afa25b6d5632
aqib javed <aqibbutt3078@gmail.com>
2024-09-06 01:02:05 +0100

    Leetcode-648 | Replace words

--- Algorithms/src/main/java/org/algorithms/io/trie/ReplaceWords.java
+++ Algorithms/src/main/java/org/algorithms/io/trie/ReplaceWords.java
@@ -1,32 +1,67 @@
 package org.algorithms.io.trie;
 
-import java.util.List;
+import java.util.*;
 
 public class ReplaceWords {
     static class Trie {
         private static final int LIMIT = 26;
         private final Trie[] nodes;
-        private int count;
         private int end;
+
         public Trie() {
             this.nodes = new Trie[LIMIT];
         }
 
         public void insert(String word) {
             var current = this;
-            for(char ch: word.toCharArray()) {
+            for (char ch : word.toCharArray()) {
                 int idx = ch - 'a';
-                if(current.nodes[idx] == null) {
+                if (current.nodes[idx] == null) {
                     current.nodes[idx] = new Trie();
                 }
                 current = current.nodes[idx];
-                current.count++;
             }
             current.end++;
         }
-    }
-    public String replaceWords(List<String> dictionary, String sentence) {
 
-        return "";
+        public String getWord(String word) {
+            var builder = new StringBuilder();
+            var current = this;
+            for (char ch : word.toCharArray()) {
+                int idx = ch - 'a';
+                if (current.nodes[idx] == null) {
+                    return word;
+                }
+                current = current.nodes[idx];
+                builder.append(ch);
+                if(current.end > 0) return builder.toString();
+            }
+
+            return builder.toString();
+        }
+
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        var builder = new StringBuilder();
+        var data = sentence.split(" ");
+        var trie = new Trie();
+
+        for (var dic : dictionary) {
+            trie.insert(dic);
+        }
+        for(var d: data) {
+            builder.append(trie.getWord(d)).append(" ");
+        }
+        return builder.substring(0, builder.length() - 1);
+    }
+
+    public static void main(String[] args) {
+        var replace = new ReplaceWords();
+        System.out.println(replace.replaceWords(
+                        List.of("a", "aa", "aaa", "aaaa"),
+                "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa"
+                )
+        );
     }
 }
