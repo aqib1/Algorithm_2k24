91462f96759333402179c243d360dc7fea2008a8
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-15 05:07:57 +0100

    Update StudentNotInRightOrder.java

--- Algorithms/src/main/java/org/algorithms/io/arrays/StudentNotInRightOrder.java
+++ Algorithms/src/main/java/org/algorithms/io/arrays/StudentNotInRightOrder.java
@@ -1,4 +1,28 @@
 package org.algorithms.io.arrays;
 
+import java.util.Arrays;
+
 public class StudentNotInRightOrder {
+    public static void main(String[] args) {
+        System.out.println(
+                studentsNotInRightOrder(
+                        new int[] {
+                                4, 3, 2, 1
+                        }
+                )
+        );
+    }
+
+    public static int studentsNotInRightOrder(int []students) {
+        int count = 0;
+        int []original = Arrays.copyOf(students, students.length);
+        Arrays.sort(students);
+
+        for(int i = 0; i < original.length; i++) {
+            if(original[i] != students[i])
+                count++;
+        }
+
+        return count;
+    }
 }
