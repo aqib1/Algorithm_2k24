f1d670b4243c05347a238ace567ba03e99adf4b1
Aqib Javed <aqibbutt3078@gmail.com>
2024-09-21 18:38:01 +0100

    Create ReverseList.java

---
+++ Algorithms/src/main/java/org/algorithms/io/linkedlist/ReverseList.java
@@ -0,0 +1,20 @@
+package org.algorithms.io.list;
+
+public class ReverseList {
+    // Time complexity O(n) space is O(1)
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode previous = null;
+        var current = head;
+
+        while(current != null) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
