82c8b6ba6e808bfdead9b7aee151889f19bbbe91
aqib javed <aqibbutt3078@gmail.com>
2024-09-24 01:02:05 +0100

    Leetcode-2519 | Count the Number of K-Big Indices

--- Algorithms/src/main/java/org/algorithms/io/amazon2k24/CountNumberOfKBigIndices.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k24/CountNumberOfKBigIndices.java
@@ -1,5 +1,6 @@
 package org.algorithms.io.amazon2k24;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -16,37 +17,35 @@ public class CountNumberOfKBigIndices {
     // Time OnLog(n)
     // Space O(2n) = O(n)
     public int kBigIndices(int[] nums, int k) {
-        var maxHeap = new PriorityQueue<Integer>(
-                Comparator.reverseOrder()
-        );
-        var leftValid = new boolean[nums.length];
-        var rightValid = new boolean[nums.length];
         var count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (maxHeap.size() == k && maxHeap.peek() < nums[i]) {
-                leftValid[i] = true;
-            }
-            maxHeap.offer(nums[i]);
-            if(maxHeap.size() > k) {
-                maxHeap.poll();
-            }
-        }
-
-        maxHeap.clear();
-
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (maxHeap.size() == k && maxHeap.peek() < nums[i]) {
-                rightValid[i] = true;
-            }
-            maxHeap.offer(nums[i]);
-            if(maxHeap.size() > k) {
-                maxHeap.poll();
-            }
-        }
+        var left = new boolean[nums.length];
+        var right = new boolean[nums.length];
+        var maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
 
         for(int i = 0; i < nums.length; i++) {
-            if(leftValid[i] && rightValid[i]) count++;
+            if(maxHeap.size() == k && maxHeap.peek() < nums[i]) {
+                left[i] = true;
+            }
+            maxHeap.offer(nums[i]);
+
+            if(maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        maxHeap.clear();
+        for(int i = nums.length - 1; i >=0 ; i--) {
+            if(maxHeap.size() == k && maxHeap.peek() < nums[i]) {
+                right[i] = true;
+            }
+            maxHeap.offer(nums[i]);
+
+            if(maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        for(int i = 0; i < left.length; i++) {
+            if(left[i] && right[i]) count++;
         }
 
         return count;
