5c5ca799a2f0107d9f725f1dc3628881b3faf4db
aqib javed <aqibbutt3078@gmail.com>
2024-10-02 01:02:05 +0100

    Leetcode-642 : Trie class with sentence count

--- Algorithms/src/main/java/org/algorithms/io/trie/AutocompleteSystem.java
+++ Algorithms/src/main/java/org/algorithms/io/trie/AutocompleteSystem.java
@@ -3,6 +3,30 @@ package org.algorithms.io.trie;
 import java.util.*;
 
 public class AutocompleteSystem {
+    static class Trie {
+        private static final int LIMIT = 256;
+        private final Trie[] nodes;
+        private final Map<String, Integer> sentenceCount;
+        private int end;
+        private int count;
+        public Trie() {
+            this.nodes = new Trie[LIMIT];
+            this.sentenceCount = new HashMap<>();
+        }
+
+        public void insert(String word, int count) {
+            var current = this;
+            for(char ch: word.toCharArray()) {
+                if(current.nodes[ch] == null) {
+                    current.nodes[ch] = new Trie();
+                }
+                current = current.nodes[ch];
+                current.sentenceCount.put(word, current.sentenceCount.getOrDefault(word, 0) + count);
+            }
+            current.end++;
+        }
+    }
+
     public AutocompleteSystem(String[] sentences, int[] times) {
 
     }
