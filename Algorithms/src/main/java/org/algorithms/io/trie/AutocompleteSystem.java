a451829fa70bdc473a3de00b44f604eaf3a0a186
aqib javed <aqibbutt3078@gmail.com>
2024-10-04 01:02:05 +0100

    Leetcode-642 : completed

--- Algorithms/src/main/java/org/algorithms/io/trie/AutocompleteSystem.java
+++ Algorithms/src/main/java/org/algorithms/io/trie/AutocompleteSystem.java
@@ -9,6 +9,7 @@ public class AutocompleteSystem {
         private final Map<String, Integer> sentenceCount;
         private int end;
         private int count;
+
         public Trie() {
             this.nodes = new Trie[LIMIT];
             this.sentenceCount = new HashMap<>();
@@ -16,8 +17,8 @@ public class AutocompleteSystem {
 
         public void insert(String word, int count) {
             var current = this;
-            for(char ch: word.toCharArray()) {
-                if(current.nodes[ch] == null) {
+            for (char ch : word.toCharArray()) {
+                if (current.nodes[ch] == null) {
                     current.nodes[ch] = new Trie();
                 }
                 current = current.nodes[ch];
@@ -27,34 +28,72 @@ public class AutocompleteSystem {
         }
     }
 
+    private static int TOP = 3;
     private String prefix;
+    private Queue<Map.Entry<String, Integer>> heap;
     private Trie trie;
+
     public AutocompleteSystem(String[] sentences, int[] times) {
         this.trie = new Trie();
         this.prefix = "";
+        this.heap = new PriorityQueue<>(
+                (e1, e2) ->
+                        Objects.equals(e1.getValue(), e2.getValue()) ?
+                                e1.getKey().compareTo(
+                                        e2.getKey()
+                                ) : Integer.compare(e2.getValue(), e1.getValue())
+        );
+
+        initData(sentences, times);
+    }
+
+    private void initData(String[] sentences, int[] times) {
+        for(int i = 0; i < times.length; i++) {
+            trie.insert(sentences[i], times[i]);
+        }
     }
 
     public List<String> input(char c) {
-        if(c == '#') {
+        if (c == '#') {
             trie.insert(prefix, 1);
             prefix = "";
             return List.of();
         }
+
+        heap.clear();
         prefix = prefix.concat(String.valueOf(c));
         var response = new ArrayList<String>();
+        var current = trie;
+
+        for(char ch: prefix.toCharArray()) {
+            if(current.nodes[ch] == null)
+                return List.of();
+            current = current.nodes[ch];
+        }
+
+        for(var key : current.sentenceCount.keySet()) {
+            heap.offer(new AbstractMap.SimpleEntry<>(
+                    key,
+                    current.sentenceCount.get(key)
+            ));
+        }
+
+        for(int i = 0; i < TOP && !heap.isEmpty(); i++) {
+            response.add(heap.poll().getKey());
+        }
 
         return response;
     }
 
     public static void main(String[] args) {
         var complete = new AutocompleteSystem(
-                new String[] {
+                new String[]{
                         "i love you",
                         "island",
                         "iroman",
                         "i love leetcode"
                 },
-                new int[] {5, 3, 2, 2}
+                new int[]{5, 3, 2, 2}
         );
 
         System.out.println(complete.input('i'));
