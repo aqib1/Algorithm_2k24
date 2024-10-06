a4f665bfce10608d51a44796ff91a0e2d8f817c8
aqib javed <aqibbutt3078@gmail.com>
2024-10-06 01:02:05 +0100

    Leetcode-648: Trie impl

--- Algorithms/src/main/java/org/algorithms/io/trie/ReplaceWords.java
+++ Algorithms/src/main/java/org/algorithms/io/trie/ReplaceWords.java
@@ -1,4 +1,32 @@
 package org.algorithms.io.trie;
 
+import java.util.List;
+
 public class ReplaceWords {
+    static class Trie {
+        private static final int LIMIT = 26;
+        private final Trie[] nodes;
+        private int count;
+        private int end;
+        public Trie() {
+            this.nodes = new Trie[LIMIT];
+        }
+
+        public void insert(String word) {
+            var current = this;
+            for(char ch: word.toCharArray()) {
+                int idx = ch - 'a';
+                if(current.nodes[idx] == null) {
+                    current.nodes[idx] = new Trie();
+                }
+                current = current.nodes[idx];
+                current.count++;
+            }
+            current.end++;
+        }
+    }
+    public String replaceWords(List<String> dictionary, String sentence) {
+
+        return "";
+    }
 }
