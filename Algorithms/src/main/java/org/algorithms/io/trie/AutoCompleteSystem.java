7624d7eb0b3c2c821b2a29a8ffedb30ed1df6607
aqib javed <aqibbutt3078@gmail.com>
2024-10-07 01:02:05 +0100

    Leetcode-642 : Trie class completion

--- Algorithms/src/main/java/org/algorithms/io/trie/AutoCompleteSystem.java
+++ Algorithms/src/main/java/org/algorithms/io/trie/AutoCompleteSystem.java
@@ -3,5 +3,13 @@ package org.algorithms.io.trie;
 public class AutoCompleteSystem {
     static class Trie {
         private static final int LIMIT = 26;
+        private final Trie[] nodes;
+        private int end;
+        private int count;
+
+        public Trie() {
+            this.nodes = new Trie[LIMIT];
+        }
+
     }
 }
