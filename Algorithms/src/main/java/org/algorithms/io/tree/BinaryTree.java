d2a5872e1d6ae553e54a3ee5bde82660d1a193f5
Aqib Javed <aqibbutt3078@gmail.com>
2024-10-13 07:15:53 +0100

    Update BinaryTree.java

--- Algorithms/src/main/java/org/algorithms/io/tree/BinaryTree.java
+++ Algorithms/src/main/java/org/algorithms/io/tree/BinaryTree.java
@@ -1,6 +1,7 @@
 package org.algorithms.io.tree;
 
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 class Node{
@@ -54,5 +55,39 @@ public class BinaryTree {
             if(current.left != null) queue.add(current.left);
             if(current.right != null) queue.add(current.right);
         }
+    }
+
+    public int maxDepthRecursive(Node root) {
+        return maxDepth(root, 0);
+    }
+
+    public int maxDepth(Node root, int depth) {
+        if(root == null) return depth;
+
+        return Math.max(
+                maxDepth(root.left, depth + 1),
+                maxDepth(root.right, depth + 1)
+        );
+    }
+
+    public int maxDepth(Node root) {
+        if(root == null) return 0;
+        int depth = 1;
+        int maxDepth = Integer.MIN_VALUE;
+        Queue<Map.Entry<Node, Integer>> queue = new LinkedList<>();
+        queue.add(Map.entry(root, depth));
+        while(!queue.isEmpty()) {
+            var current = queue.poll();
+            maxDepth = Math.max(maxDepth, current.getValue());
+
+            if(current.getKey().left != null) queue.add(
+                    Map.entry(current.getKey().left, current.getValue() + 1)
+            );
+            if(current.getKey().right != null) queue.add(
+                    Map.entry(current.getKey().right, current.getValue() + 1)
+            );
+        }
+
+        return maxDepth;
     }
 }
