19e770d717d075dfdd9c826800198dfed8f6e098
Aqib Javed <aqibbutt3078@gmail.com>
2024-10-13 02:23:21 +0100

    Update IsBSTTree.java

--- Algorithms/src/main/java/org/algorithms/io/tree/IsBSTTree.java
+++ Algorithms/src/main/java/org/algorithms/io/tree/IsBSTTree.java
@@ -5,19 +5,22 @@ import java.util.List;
 import java.util.Objects;
 
 public class IsBSTTree {
+    record Node1(int data, Node1 left, Node1 right) {
+
+    }
     private final List<Integer> inOrder;
     public IsBSTTree() {
         this.inOrder = new ArrayList<>();
     }
 
-    public void inOrder(Node root) {
+    public void inOrder(Node1 root) {
         if(Objects.isNull(root)) return;
         inOrder(root.left());
         inOrder.add(root.data());
         inOrder(root.right());
     }
 
-    public boolean isBST(Node root) {
+    public boolean isBST(Node1 root) {
         inOrder(root);
         for(int i = 0; i < inOrder.size() - 1; i++) {
             if(inOrder.get(i) >= inOrder.get(i + 1))
