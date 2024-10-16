a7ab9d6558d9bf6b56ccd5c17cdb6fc149c42500
Aqib Javed <aqibbutt3078@gmail.com>
2024-10-16 01:56:27 +0100

    Create GetRunnable.java

---
+++ Algorithms/src/main/java/org/algorithms/io/threading/GetRunnable.java
@@ -0,0 +1,44 @@
+package org.algorithms.io.threading;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class GetRunnable implements Runnable {
+
+    private final AtomicInteger random = new AtomicInteger();
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(10000);
+            random.set(ThreadLocalRandom.current().nextInt(1, 1000));
+            synchronized (this) {
+                notifyAll();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Integer get() throws InterruptedException {
+        synchronized (this) {
+            if (random.get() == 0) {
+                wait();
+            }
+        }
+        return random.get();
+    }
+}
+
+//FutureTask like behaviour
+public class ThreadWaitAndNotifyAll {
+    public static void main(String[] args) throws InterruptedException {
+        var getRunnable = new GetRunnable();
+        var thread = new Thread(getRunnable);
+        thread.start();
+
+        // blocking call.
+        System.out.println(getRunnable.get());
+        System.out.println("Bye");
+    }
+}
