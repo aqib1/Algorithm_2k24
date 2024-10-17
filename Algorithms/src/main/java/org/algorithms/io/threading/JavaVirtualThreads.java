3d6d292bdddf1ac2df9c60f58bbff41a794a2579
Aqib Javed <aqibbutt3078@gmail.com>
2024-10-17 22:32:06 +0100

    Create JavaVirtualThreads.java

---
+++ Algorithms/src/main/java/org/algorithms/io/threading/JavaVirtualThreads.java
@@ -0,0 +1,48 @@
+package org.algorithms.io.threading;
+
+//import java.util.ArrayList;
+
+public class JavaVirtualThreads {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread.ofVirtual()
+                .name("Aqib")
+                .start(() -> {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Hi there");
+                })
+                .join();
+
+
+//        var virtualThreads = new ArrayList<Thread>();
+//
+//        for(int i = 0; i < 10000; i++) {
+//            virtualThreads.add(Thread.startVirtualThread(() -> {
+//                try {
+//                    Thread.sleep(1);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                System.out.println("Hi there" );
+//            }));
+//        }
+//
+//        virtualThreads.forEach(thread -> {
+//            try {
+//                thread.join();
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+//
+//        String data = "Aqib";
+//        switch(data) {
+//            case "Aqib", "aqib" -> System.out.println("Hi there");
+//            default -> System.out.println("Bye there");
+//        }
+    }
+}
