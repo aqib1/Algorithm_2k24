f8a5679836d3eac6054f6215b4286b9d794a9687
Aqib Javed <aqibbutt3078@gmail.com>
2024-10-26 20:40:26 +0100

    Update MyCircularQueue.java

--- Algorithms/src/main/java/org/algorithms/io/amazon2k24/MyCircularQueue.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k24/MyCircularQueue.java
@@ -20,11 +20,14 @@ public class MyCircularQueue {
     private int front;
     private int rare;
     private int size;
+
+    // Space complexity O(k)
     public MyCircularQueue(int k) {
         this.queue = new int[k];
         this.rare = -1;
     }
 
+    // time complexity O(1)
     public boolean enQueue(int value) {
         if(isFull()) return false;
 
@@ -34,7 +37,7 @@ public class MyCircularQueue {
         return true;
     }
 
-
+    // time complexity O(1)
     public boolean deQueue() {
         if(isEmpty()) return false;
 
@@ -43,18 +46,22 @@ public class MyCircularQueue {
         return true;
     }
 
+    // time complexity O(1)
     public int Front() {
         return isEmpty() ? -1 : this.queue[front];
     }
 
+    // time complexity O(1)
     public int Rear() {
         return isEmpty() ? -1 : this.queue[rare];
     }
 
+    // time complexity O(1)
     public boolean isEmpty() {
         return size == 0;
     }
 
+    // time complexity O(1)
     public boolean isFull() {
         return size == queue.length;
     }
