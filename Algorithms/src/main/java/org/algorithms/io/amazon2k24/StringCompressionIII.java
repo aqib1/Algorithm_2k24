1140d8e6848ab4ff39802e5abd96ab343613b6e0
Aqib Javed <aqibbutt3078@gmail.com>
2024-11-04 01:50:53 +0000

    Create StringCompressionIII.java

---
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k24/StringCompressionIII.java
@@ -0,0 +1,30 @@
+package org.algorithms.io.amazon2k24;
+
+public class StringCompressionIII {
+    public static void main(String[] args) {
+        System.out.println(
+                compressedString("aaaaaaaaay")
+        );
+    }
+    public static String compressedString(String word) {
+        if(word.isEmpty())
+            return "";
+
+        var wordData = word.toCharArray();
+        StringBuilder compressed = new StringBuilder();
+        int position = 0;
+        while(position < wordData.length) {
+            int count = 0;
+            var current = wordData[position];
+            while(position < wordData.length
+                    && current == wordData[position]
+                        && count < 9) {
+                count++;
+                position++;
+            }
+            compressed.append(count).append(current);
+        }
+
+        return compressed.toString();
+    }
+}
