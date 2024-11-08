1a8daf449582aa5f95203f8d81b9769fce71d761
aqib javed <aqibbutt3078@gmail.com>
2024-11-08 01:02:05 +0000

    WordSearchII construct trie class

--- Algorithms/src/main/java/org/algorithms/io/amazon2k24/revision/WordSearchII.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k24/revision/WordSearchII.java
@@ -1,4 +1,14 @@
 package org.algorithms.io.amazon2k24.revision;
 
 public class WordSearchII {
+    static class Trie {
+        private static final int LIMIT = 26;
+        private final Trie[] nodes;
+        private int count;
+        private int end;
+
+        public Trie() {
+            this.nodes = new Trie[LIMIT];
+        }
+    }
 }
