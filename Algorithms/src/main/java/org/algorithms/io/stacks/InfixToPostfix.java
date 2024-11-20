60b4c864280c5fbcd1088bd6aed423481f51709d
Aqib Javed <aqibbutt3078@gmail.com>
2024-11-20 04:42:56 +0000

    Update InfixToPostfix.java

--- Algorithms/src/main/java/org/algorithms/io/stacks/InfixToPostfix.java
+++ Algorithms/src/main/java/org/algorithms/io/stacks/InfixToPostfix.java
@@ -1,85 +1,86 @@
 package org.algorithms.io.stacks;
 
+
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
+
+class Node {
+    char val;
+    Node left;
+    Node right;
+
+    Node() {
+        this.val = ' ';
+    }
+
+    Node(char val) {
+        this.val = val;
+    }
+
+    Node(char val, Node left, Node right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 public class InfixToPostfix {
     public static void main(String[] args) {
-        String infix = "(1+(4+5+2)-3)+(6+8)";
+        String infix = "2-3/(5*2)+1";
         System.out.println(
-                calculatePostfix(
-                        infixToPostfix(infix)
-                )
+                infixToPostfix(infix)
         );
     }
 
-    private static int calculatePostfix(String postfix) {
-        var operandStack = new Stack<Integer>();
-        for (char ch : postfix.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                operandStack.push(Integer.parseInt("" + ch));
-            } else {
-                int second = operandStack.pop();
-                int first = operandStack.pop();
-                int result = operation(first, second, ch);
-                operandStack.push(result);
-            }
-        }
-
-        return operandStack.pop();
-    }
-
-    private static int operation(int first, int second, char ch) {
-        return switch (ch) {
-            case '+' -> first + second;
-            case '-' -> first - second;
-            case '*' -> first * second;
-            case '/' -> first / second;
-            default -> 0;
-        };
-    }
-
-    private static String infixToPostfix(String infix) {
+    // Time complexity O(n) where n is the number of character
+    // Space complexity O(n)
+    public static String infixToPostfix(String exp) {
         var postfix = new StringBuilder();
-        var operatorStack = new Stack<Character>();
-
-        for (char ch : infix.toCharArray()) {
-            if (Character.isDigit(ch)) {
+        var operatorsStack = new Stack<Character>();
+        for (char ch : exp.toCharArray()) {
+            if (isDigit(ch)) {
                 postfix.append(ch);
             } else {
-                if (!operatorStack.isEmpty()) {
-                    var opTop = operatorStack.pop();
-                    if (opTop == '(') {
-                        operatorStack.push(opTop);
-                    } else if (precedence(opTop) > precedence(ch) && precedence(ch) != -1) {
-                        postfix.append(opTop);
-                    } else {
-                        operatorStack.push(opTop);
-                    }
+                // keep appending operator until have high precedence then current char
+                while (!operatorsStack.isEmpty()
+                        && precedence(ch) != -1
+                        && precedence(operatorsStack.peek()) >= precedence(ch)) {
+                    postfix.append(operatorsStack.pop());
                 }
+
+                // in case of brackets keep tracking backwards until found start of bracket
                 if (ch == ')') {
-                    char pop;
-                    while ((pop = operatorStack.pop()) != '(') {
-                        postfix.append(pop);
+                    char opChar;
+                    while (!operatorsStack.isEmpty() && (opChar = operatorsStack.pop()) != '(') {
+                        // append to result
+                        postfix.append(opChar);
                     }
                 } else {
-                    operatorStack.add(ch);
+                    operatorsStack.push(ch); // adding current operator to stack
                 }
             }
         }
 
-        while (!operatorStack.isEmpty()) {
-            postfix.append(operatorStack.pop());
+        while (!operatorsStack.isEmpty()) {
+            postfix.append(operatorsStack.pop());
         }
 
         return postfix.toString();
     }
 
-    private static int precedence(char operator) {
-        return switch (operator) {
-            case '/' -> 3;
-            case '*' -> 2;
-            case '+' -> 1;
-            case '-' -> 0;
+    public static boolean isDigit(char ch) {
+        return ch != '^' &&
+                ch != '*' && ch != '/'
+                && ch != '-' && ch != '+'
+                && ch != '(' && ch != ')';
+    }
+
+    public static int precedence(char ch) {
+        return switch (ch) {
+            case '^' -> 3;
+            case '*', '/' -> 2;
+            case '+', '-' -> 1;
             default -> -1;
         };
     }
