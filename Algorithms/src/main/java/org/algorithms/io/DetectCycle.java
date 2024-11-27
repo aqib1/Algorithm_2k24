50bec82256581b72e991524c4accbc7492497032
aqib javed <aqibbutt3078@gmail.com>
2024-11-27 01:32:27 +0000

    Hackerrank | Find a cycle in singly linked list

---
+++ Algorithms/src/main/java/org/algorithms/io/DetectCycle.java
@@ -0,0 +1,26 @@
+package org.algorithms.io;
+
+public class DetectCycle {
+    static class Node {
+        int data;
+        Node next;
+    }
+
+    boolean hasCycle(Node head) {
+        if(head == null)
+            return false;
+
+        Node fast = head;
+        Node slow = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow)
+                return true;
+        }
+
+        return false;
+    }
+}
