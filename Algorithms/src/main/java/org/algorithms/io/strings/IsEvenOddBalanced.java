9c3406f84e4298ea6fdb01950699d5d72c575685
aqib javed <aqibbutt3078@gmail.com>
2024-12-05 20:21:35 +0000

    Leetcode 3340 | Is balanced string

---
+++ Algorithms/src/main/java/org/algorithms/io/strings/IsEvenOddBalanced.java
@@ -0,0 +1,22 @@
+package org.algorithms.io.strings;
+
+public class IsEvenOddBalanced {
+    public static void main(String[] args) {
+        System.out.println('2' - '0');
+    }
+
+    public boolean isBalanced(String num) {
+        int evenSum = 0, oddSum = 0;
+
+        for(int i = 0; i < num.length(); i++) {
+            int n = num.charAt(i) - '0';
+            if(i % 2 == 0) {
+                evenSum += n;
+            } else {
+                oddSum += n;
+            }
+        }
+
+        return evenSum == oddSum;
+    }
+}
