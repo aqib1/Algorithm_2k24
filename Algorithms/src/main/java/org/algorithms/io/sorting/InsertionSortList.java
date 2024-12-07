076edb74a13cb896a4bf4b101d6b8a9dc2a6816d
aqib javed <aqibbutt3078@gmail.com>
2024-12-07 23:59:57 +0000

    Leetcode 147 | Insertion Sort list

--- Algorithms/src/main/java/org/algorithms/io/sorting/InsertionSortList.java
+++ Algorithms/src/main/java/org/algorithms/io/sorting/InsertionSortList.java
@@ -4,25 +4,26 @@ import org.algorithms.io.linkedlist.ListNode;
 
 public class InsertionSortList {
     /*
-    * Time complexity O(n)2 + Space O(n)
+    * Time complexity O(n)2 + Space O(1)
     * */
     public ListNode insertionSortList(ListNode head) {
         if(head.next == null)
             return head;
-        var sorted = new ListNode(Integer.MIN_VALUE);
-        var nextValue = head;
-        while(nextValue != null) {
-            var sortPtr = sorted;
-            while (sortPtr.next != null && nextValue.val >= sortPtr.next.val) {
-                sortPtr = sortPtr.next;
+        var dummy = new ListNode(Integer.MIN_VALUE);
+        var current = head;
+
+        while(current != null) {
+            var prev = dummy;
+            while(prev.next != null && prev.next.val < current.val) {
+                prev = prev.next;
             }
 
-            var next = sortPtr.next;
-            sortPtr.next = new ListNode(nextValue.val);
-            sortPtr.next.next = next;
-            nextValue = nextValue.next;
+            var next = current.next;
+            current.next = prev.next;
+            prev.next = current;
+            current = next;
         }
 
-        return sorted.next;
+        return dummy.next;
     }
 }
