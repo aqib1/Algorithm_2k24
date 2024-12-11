46751b5c8bef61050e0fb00b77cd9436187baf81
aqib javed <aqibbutt3078@gmail.com>
2024-12-11 01:02:03 +0000

    Leetcode 76 | Init min window found in string

---
+++ Algorithms/src/main/java/org/algorithms/io/strings/MinWindow76.java
@@ -0,0 +1,26 @@
+package org.algorithms.io.strings;
+
+/**
+ * Given two strings s and t of lengths m and n respectively, return the minimum window
+ * substring
+ *  of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+ *
+ * The testcases will be generated such that the answer is unique.
+ * Example 1:
+ * Input: s = "ADOBECODEBANC", t = "ABC"
+ * Output: "BANC"
+ * Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+ * Example 2:
+ * Input: s = "a", t = "a"
+ * Output: "a"
+ * Explanation: The entire string s is the minimum window.
+ * */
+
+
+public class MinWindow76 {
+    public String minWindow(String s, String t) {
+
+
+        return "";
+    }
+}
