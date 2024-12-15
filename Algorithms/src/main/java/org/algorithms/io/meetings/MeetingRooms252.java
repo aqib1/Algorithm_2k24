ac69a21388ea4bb472dca60d226fc2edc2f71f43
aqib javed <aqibbutt3078@gmail.com>
2024-12-15 06:19:34 +0000

    Leetcode 252 | A person can attend all meetings

---
+++ Algorithms/src/main/java/org/algorithms/io/meetings/MeetingRooms252.java
@@ -0,0 +1,34 @@
+package org.algorithms.io.meetings;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MeetingRooms252 {
+    public static void main(String[] args) {
+        var meeting = new MeetingRooms252();
+        System.out.println(
+                meeting.canAttendMeetings(
+                        new int[][] {
+                                {7, 10},
+                                {2, 4}
+                        }
+                )
+        );
+    }
+
+    // Time complexity ON(logN)
+    // Space O(1)
+    public boolean canAttendMeetings(int[][] intervals) {
+        if(intervals.length == 0)
+            return true;
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+
+        for(int i = 1; i < intervals.length; i++) {
+            if(intervals[i - 1][1] > intervals[i][0]) return false;
+        }
+
+        return true;
+    }
+}
