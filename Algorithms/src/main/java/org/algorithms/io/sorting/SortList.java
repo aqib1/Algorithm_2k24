2380c6da8172db6b9739b69dfb27347b41335f2d
aqib javed <aqibbutt3078@gmail.com>
2024-12-18 05:08:21 +0000

    Insertion sort to LinkedList

---
+++ Algorithms/src/main/java/org/algorithms/io/sorting/SortList.java
@@ -0,0 +1,28 @@
+package org.algorithms.io.sorting;
+
+import org.algorithms.io.linkedlist.ListNode;
+
+import java.util.Objects;
+
+public class SortList {
+
+    public ListNode sortList(ListNode head) {
+        if(Objects.isNull(head))
+            return null;
+        var dummy = new ListNode(Integer.MIN_VALUE);
+        var current = head;
+
+        while(current != null) {
+            var previous = dummy;
+            while(previous.next != null && current.val < previous.next.val) {
+                previous = previous.next;
+            }
+            var next = current.next;
+            current.next = previous.next;
+            previous.next = current;
+            current  = next;
+        }
+
+        return dummy.next;
+    }
+}
