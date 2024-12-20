e74965b3b2ef41304c2b7ddcfbde8dba5d19835f
aqib javed <aqibbutt3078@gmail.com>
2024-12-20 11:51:37 +0000

    Leetcode 1143 | Longest common subsequence time bottom up dynamic programming

--- Algorithms/src/main/java/org/algorithms/io/strings/LongestCommonSubsequence.java
+++ Algorithms/src/main/java/org/algorithms/io/strings/LongestCommonSubsequence.java
@@ -5,15 +5,35 @@ public class LongestCommonSubsequence {
     public static void main(String[] args) {
         var lcs = new LongestCommonSubsequence();
         System.out.println(
-           lcs.longestCommonSubsequence(
+           lcs.longestCommonSubsequenceMemoization(
                    "abcde",
                    "abc"
            )
         );
     }
+
+    // time complexity O(n * m)
+    // Space complexity O(n * m)
+    public int longestCommonSubsequenceDp(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int [][] dp = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j- 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
     // Time complexity O(m * n)
     // Space complexity due to memoization O(m * n)
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int longestCommonSubsequenceMemoization(String text1, String text2) {
         return lcs(
                 text1,
                 text2,
