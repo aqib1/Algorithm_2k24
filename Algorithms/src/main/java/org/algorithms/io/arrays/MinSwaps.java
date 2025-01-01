475c613a6cd821b6819a8086c015c8ea992077c6
aqib javed <aqibbutt3078@gmail.com>
2025-01-01 23:24:21 +0000

    Minimum swaps to group all 1s together

--- Algorithms/src/main/java/org/algorithms/io/arrays/MinSwaps.java
+++ Algorithms/src/main/java/org/algorithms/io/arrays/MinSwaps.java
@@ -1,6 +1,8 @@
 package org.algorithms.io.arrays;
 
 public class MinSwaps {
+    // Time complexity O(n)
+    // Space complexity O(1)
     public int minSwaps(int[] data) {
         int windowSize = 0;
         for(int i : data) windowSize += i;
