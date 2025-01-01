17ae71309d8bb7a18e868621c302e2e468bfe109
aqib javed <aqibbutt3078@gmail.com>
2025-01-01 02:27:38 +0000

    Find kth largest element

---
+++ Algorithms/src/main/java/org/algorithms/io/heap/FindKthLargest.java
@@ -0,0 +1,31 @@
+package org.algorithms.io.heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class FindKthLargest {
+    public static void main(String[] args) {
+        var f = new FindKthLargest();
+        System.out.println(
+                f.findKthLargest(
+                        new int[] {11, 12, 13, 14, 2},
+                        3
+                )
+        );
+    }
+
+    // Time complexity OnLog(k)
+    // Space complexity O(k)
+    public int findKthLargest(int[] nums, int k) {
+        var minHeap = new PriorityQueue<Integer>(k);
+        for(int num: nums) {
+            if(minHeap.size() == k && num > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(num);
+            } else if(minHeap.size() != k) {
+                minHeap.offer(num);
+            }
+        }
+        return minHeap.isEmpty() ? -1: minHeap.poll();
+    }
+}
