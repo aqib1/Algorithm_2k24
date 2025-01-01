0d349a5a3a0f59d3906bc779a61cc0fd39d6d8a6
aqib javed <aqibbutt3078@gmail.com>
2025-01-01 23:23:22 +0000

    Minimum swaps to group all 1s together

---
+++ Algorithms/src/main/java/org/algorithms/io/arrays/MinSwaps.java
@@ -0,0 +1,21 @@
+package org.algorithms.io.arrays;
+
+public class MinSwaps {
+    public int minSwaps(int[] data) {
+        int windowSize = 0;
+        for(int i : data) windowSize += i;
+
+        int currOnesInWindow = 0;
+        int maxOnesInWindows = Integer.MIN_VALUE;
+
+        for(int i = 0; i < data.length; i++) {
+            currOnesInWindow += data[i];
+            if(i >= windowSize) {
+                currOnesInWindow -= data[i - windowSize];
+            }
+            maxOnesInWindows = Math.max(maxOnesInWindows, currOnesInWindow);
+        }
+
+        return windowSize - maxOnesInWindows;
+    }
+}
