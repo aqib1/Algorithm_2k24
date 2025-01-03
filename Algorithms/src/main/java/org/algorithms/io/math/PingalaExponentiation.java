aea7cd8d677598edc8042fd793b7c5dd7420afc8
aqib javed <aqibbutt3078@gmail.com>
2025-01-03 20:45:17 +0000

    Find pow using pingala exponentiation algorithm

---
+++ Algorithms/src/main/java/org/algorithms/io/math/PingalaExponentiation.java
@@ -0,0 +1,20 @@
+package org.algorithms.io.math;
+
+public class PingalaExponentiation {
+    // time complexity OLog(n)
+    // space complexity O(1)
+    public double pow(int a, long pow) {
+        if(pow == 0)
+            return 1;
+        else if(pow == 1.0)
+            return a;
+        else if(pow < 0)
+            return 1 / pow(a, -pow);
+        else {
+            double x = pow(a, pow / 2);
+            if(pow % 2 == 0)
+                return x * x;
+            else return x * x * a;
+        }
+    }
+}
