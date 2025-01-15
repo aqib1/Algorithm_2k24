9899aee498548b08d865be7189d525fb4b5d3a0d
aqib javed <aqibbutt3078@gmail.com>
2025-01-16 01:02:03 +0500

    Build tree using post and in order traversal array

---
+++ Algorithms/src/main/java/org/algorithms/io/binarytree/ConstructBinaryTreeFromOrder.java
@@ -0,0 +1,34 @@
+package org.algorithms.io.binarytree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstructBinaryTreeFromOrder {
+    private int[] postorder;
+    private int postorderIndex;
+    private Map<Integer, Integer> inorderIndex;
+
+    // Time complexity O(n)
+    // Space complexity O(n)
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        this.inorderIndex = new HashMap<>();
+        this.postorder = postorder;
+        postorderIndex = postorder.length - 1;
+        int index = 0;
+        for(int in : inorder) inorderIndex.put(in, index++);
+
+        return helper(0, inorder.length - 1);
+    }
+
+    private TreeNode helper(int left, int right) {
+        if(left > right)
+            return null;
+
+        TreeNode rootNode = new TreeNode(postorder[postorderIndex]);
+        int rootIndex = inorderIndex.get(rootNode.val);
+        postorderIndex--;
+        rootNode.right = helper(rootIndex + 1, right);
+        rootNode.left = helper(left, rootIndex - 1);
+        return rootNode;
+    }
+}
