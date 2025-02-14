8226f3826dd23dc2de00cb9c53f865b3455d4de3
aqib javed <aqibbutt3078@gmail.com>
2025-02-14 21:19:55 +0000

    Fibonacci using head recursion

---
+++ Algorithms/src/main/java/org/algorithms/io/recursion/FibonacciUsingHeadRecursion.java
@@ -0,0 +1,16 @@
+package org.algorithms.io.recursion;
+
+public class FibonacciUsingHeadRecursion {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(8));
+    }
+    public static int fibonacci(int n) {
+        if(n == 0 || n == 1)
+            return n;
+
+        int last = fibonacci(n - 1);
+        int secondLast = fibonacci(n - 2);
+
+        return last + secondLast;
+    }
+}
