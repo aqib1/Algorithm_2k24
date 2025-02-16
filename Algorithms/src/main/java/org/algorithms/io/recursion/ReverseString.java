30be8010d2dcb6cc1cd4d7c3b95bdccd88825306
aqib javed <aqibbutt3078@gmail.com>
2025-02-16 19:10:52 +0000

    Reverse string using recursion

---
+++ Algorithms/src/main/java/org/algorithms/io/recursion/ReverseString.java
@@ -0,0 +1,37 @@
+package org.algorithms.io.recursion;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        var reverse = new ReverseString();
+        System.out.println(reverse.reverse("Kevin"));
+    }
+
+    public String reverse(String s) {
+        return reverseTailRecursion(s, "", s.length() - 1);
+    }
+
+    public String reverseTailRecursion(
+            String input,
+            String accum,
+            int index) {
+        if(index < 0) {
+            return accum;
+        }
+
+        return reverseTailRecursion(
+                input,
+                accum + input.charAt(index),
+                index - 1
+        );
+    }
+
+    // Head recursion
+    public String reverseBacktracking(String input, String output, int index) {
+        if(index >= input.length()) {
+            return output;
+        }
+        var result = reverseBacktracking(input, output, index + 1);
+        result += input.charAt(index);
+        return result;
+    }
+}
