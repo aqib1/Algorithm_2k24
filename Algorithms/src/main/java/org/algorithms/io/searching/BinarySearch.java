5cb67057ef0db16ff554c01546f544f8990f006d
aqib javed <aqibbutt3078@gmail.com>
2025-02-16 21:25:11 +0000

    Binary Search using recursion - O(n)

--- Algorithms/src/main/java/org/algorithms/io/searching/BinarySearch.java
+++ Algorithms/src/main/java/org/algorithms/io/searching/BinarySearch.java
@@ -4,10 +4,27 @@ import java.util.Arrays;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(binarySearch(
+        System.out.println(binarySearchRec(
                 new int[]{3, 1, 2, 11, 8, 12},
-                123
+                2
         ));
+    }
+
+    public static int binarySearchRec(int[] container, int item) {
+        Arrays.sort(container);
+
+        return binarySearchRec(container, item, 0, container.length - 1);
+    }
+
+    public static int binarySearchRec(int[] container, int item, int left, int right) {
+        if(right < left)
+            return -1;
+        int mid = left + (right - left) / 2;
+        if(container[mid] == item) return item;
+
+        if(item >= container[mid])
+            return binarySearchRec(container, item, mid + 1 , right);
+        else return binarySearchRec(container, item, left, mid - 1);
     }
 
     public static int binarySearch(int[] container, int item) {
