b37c4a1f1b75b0a459f89f8578eba575a61668f3
aqib javed <aqibbutt3078@gmail.com>
2025-02-19 21:40:36 +0000

    Find kth largest number using introselect algorithm a hybrid algorithm of quick-select and median of medians

---
+++ Algorithms/src/main/java/org/algorithms/io/sorting/FindKthLargestBigInteger.java
@@ -0,0 +1,102 @@
+package org.algorithms.io.sorting;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class FindKthLargestBigInteger {
+    public static void main(String[] args) {
+        var f = new FindKthLargestBigInteger();
+        System.out.println(
+            f.kthLargestNumber(
+               new String[] {"3","6","7","10"},
+               1
+            )
+        );
+    }
+    public String kthLargestNumber(String[] nums, int k) {
+        if (k < 1 || k > nums.length)
+            throw new IllegalArgumentException("k is out of bounds");
+
+        List<BigInteger> data = new ArrayList<>(Arrays.stream(nums).map(BigInteger::new).toList());
+        return quickSelect(data, 0, nums.length - 1, nums.length - k);
+    }
+
+    public static String quickSelect(List<BigInteger> nums, int start, int end, int k) {
+        if(start == end) {
+            return nums.get(start).toString();
+        }
+
+        int partition = lomutoPartition(nums, start, end, medianOfMedians(nums, start, end));
+
+        if(partition == k) {
+            return nums.get(partition).toString();
+        }
+
+        if(k < partition) {
+            return quickSelect(nums, start, partition - 1, k);
+        } else {
+            return quickSelect(nums, partition + 1, end, k);
+        }
+    }
+
+    public static void swap(List<BigInteger> nums, int i, int j) {
+        BigInteger tmp = nums.get(i);
+        nums.set(i, nums.get(j));
+        nums.set(j, tmp);
+    }
+
+    public static int lomutoPartition(List<BigInteger> nums, int low, int high, BigInteger pivotValue) {
+        int pivotIndex = 0;
+        for(int i = 0; i < nums.toArray().length; i++) {
+            if(pivotValue.equals(nums.get(i))) {
+                pivotIndex = i;
+            }
+        }
+        swap(nums, pivotIndex, high);
+        int i = low - 1;
+        for(int j = low; j <= high; j++) {
+            if(nums.get(j).compareTo(pivotValue) < 0) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, high);
+        return i + 1;
+    }
+
+    public static void insertionSort(List<BigInteger> nums, int start, int end) {
+        for(int ptr = start + 1; ptr <= end; ptr++) {
+            var data = nums.get(ptr);
+            int j = ptr - 1;
+            while(j >= 0 && nums.get(j).compareTo(data) > 0) {
+                nums.set(j + 1, nums.get(j));
+                j--;
+            }
+            nums.set(j + 1, data);
+        }
+    }
+
+    public static BigInteger medianOfMedians(List<BigInteger> nums, int start, int end) {
+        int n = end - start + 1;
+        if(n <= 5) {
+            insertionSort(nums, start, end);
+            return nums.get(start + n / 2);
+        }
+
+        int size = (n + 4) / 5;
+        List<BigInteger> medians = new ArrayList<>(size);
+        for(int i = 0; i < size; i++) {
+            int subLeft = start + i * 5;
+            int subEnd = Math.min(subLeft + 4, end);
+
+            insertionSort(nums, subLeft, subEnd);
+
+            medians.add(nums.get(subLeft + (subEnd - subLeft) / 2));
+        }
+
+        return medianOfMedians(medians, 0, size - 1);
+    }
+}
