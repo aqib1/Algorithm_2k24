d2725eb1c7dcadf362d90352211d586f545e2694
aqib javed <aqibbutt3078@gmail.com>
2025-03-01 01:02:03 +0000

    Practice examples

---
+++ Algorithms/src/main/java/org/algorithms/io/backtracking/HamiltonianCycleI.java
@@ -0,0 +1,67 @@
+package org.algorithms.io.backtracking;
+
+
+public class HamiltonianCycleI {
+
+    public static void main(String[] args) {
+        var hm = new HamiltonianCycleI();
+        System.out.println(hm.check(new int[][]{
+                {0, 1, 0, 0, 0, 1},
+                {1, 0, 1, 0, 0, 0},
+                {0, 1, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 1},
+                {0, 0, 1, 1, 0, 1},
+                {1, 0, 0, 1, 1, 0}
+        }));
+        hm.printPath();
+    }
+
+    private int[][] adjacencyMatrix;
+    private int[] hamiltonPath;
+    private int noOfVertexes;
+
+    boolean check(int[][] adjacencyMatrix) {
+        this.adjacencyMatrix = adjacencyMatrix;
+        this.noOfVertexes = adjacencyMatrix[0].length;
+        this.hamiltonPath = new int[noOfVertexes];
+        hamiltonPath[0] = 0;
+        return findHamiltonCycle(1);
+    }
+
+    private boolean findHamiltonCycle(int position) {
+        if(position == noOfVertexes) {
+            return adjacencyMatrix[hamiltonPath[position - 1]][hamiltonPath[0]] == 1;
+        }
+        for(int vertex = 1; vertex < noOfVertexes; vertex++) {
+            if(isValid(vertex, position)) {
+                hamiltonPath[position] = vertex;
+                if(findHamiltonCycle(position + 1)) {
+                    return true;
+                }
+
+                // BACKTRACK
+            }
+        }
+        return false;
+    }
+
+    private boolean isValid(int vertex, int position) {
+        if(adjacencyMatrix[hamiltonPath[position - 1]][vertex] == 0) {
+            return false;
+        }
+
+        for(int i = 0; i < position; i++) {
+            if(hamiltonPath[i] == vertex)
+                return false;
+        }
+
+        return true;
+    }
+
+    public void printPath() {
+        for (int j : hamiltonPath) {
+            System.out.print(j + " - ");
+        }
+        System.out.println(hamiltonPath[0]);
+    }
+}
---
+++ Algorithms/src/main/java/org/algorithms/io/google/MergeAlternative.java
@@ -0,0 +1,24 @@
+package org.algorithms.io.google;
+
+public class MergeAlternative {
+    // Time complexity O(n + m) ~= O(n)
+    // Space complexity O(n + m) ~= O(n)
+    public String mergeAlternately(String word1, String word2) {
+        var merged = new char[word1.length() + word2.length()];
+        int mergeIndex = 0, w1 = 0, w2 = 0;
+        while(w1 < word1.length() && w2 < word2.length()) {
+            merged[mergeIndex++] = word1.charAt(w1++);
+            merged[mergeIndex++] = word2.charAt(w2++);
+        }
+
+        while(w1 < word1.length()) {
+            merged[mergeIndex++] = word1.charAt(w1++);
+        }
+
+        while(w2 < word2.length()) {
+            merged[mergeIndex++] = word2.charAt(w2++);
+        }
+
+        return new String(merged);
+    }
+}
