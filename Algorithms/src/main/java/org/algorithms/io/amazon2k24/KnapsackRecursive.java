4aca326885403995ed36a5efe04ae39727d5c1a8
aqib javed <aqibbutt3078@gmail.com>
2025-03-09 01:02:03 +0000

    Knapsack | Using Recursion brute force

---
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k24/KnapsackRecursive.java
@@ -0,0 +1,30 @@
+package org.algorithms.io.amazon2k24;
+
+
+public class KnapsackRecursive {
+    public static void main(String[] args) {
+        var k = new KnapsackRecursive();
+        System.out.println(
+                k.knapsack(4, new int[] {1, 2, 3}, new int[] {4, 5, 1})
+        );
+    }
+
+    // Time complexity O(2^n) and space O(2^n)
+    public int knapsack(int w, int[] val, int[] wt) {
+        return knapsack(w, val, wt, val.length);
+    }
+
+    public int knapsack(int w, int[] val, int[] wt, int n) {
+        if(w == 0 || n == 0)
+            return 0;
+
+        int pickValue = 0;
+        if(wt[n - 1] <= w) {
+            pickValue = val[n - 1] + knapsack(w - wt[n - 1], val, wt, n - 1);
+        }
+
+        int notPick = knapsack(w, val, wt, n - 1);
+
+        return Math.max(pickValue, notPick);
+    }
+}
