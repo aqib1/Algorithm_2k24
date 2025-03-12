497cca3d09586c5ad8de5e8a745e598d1c27dfa7
aqib javed <aqibbutt3078@gmail.com>
2025-03-12 01:02:03 +0000

    Bottom up tabulation approach to find fibonaci numbers

--- Algorithms/src/main/java/org/algorithms/io/dynamic_programming/FibonacciMemoization.java
+++ Algorithms/src/main/java/org/algorithms/io/dynamic_programming/FibonacciMemoization.java
@@ -3,7 +3,8 @@ package org.algorithms.io.dynamic_programming;
 public class FibonacciMemoization {
     public static void main(String[] args) {
         var f = new FibonacciMemoization();
-        System.out.println(f.fib(0));
+        System.out.println(f.fib(5));
+        System.out.println(f.fibTabulation(5));
     }
 
     // Top Down Memoization approach
@@ -14,16 +15,31 @@ public class FibonacciMemoization {
         int[] memoization = new int[n + 1];
         memoization[0] = 0;
         memoization[1] = 1;
-        return fib(n, memoization);
+        return fibMemoization(n, memoization);
     }
 
-    public int fib(int n, int[] memoization) {
+    public int fibMemoization(int n, int[] memoization) {
         if(n == 0 || n == 1)
             return n;
         if(memoization[n] != 0)
             return memoization[n];
 
-        memoization[n] = fib(n - 1, memoization) + fib(n - 2, memoization);
+        memoization[n] = fibMemoization(n - 1, memoization) + fibMemoization(n - 2, memoization);
         return memoization[n];
+    }
+
+    public int fibTabulation(int n) {
+        if(n == 0)
+            return n;
+
+        var tabulation = new int[n + 1];
+        tabulation[0] = 0;
+        tabulation[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            tabulation[i] = tabulation[i - 1] + tabulation[i - 2];
+        }
+
+        return tabulation[n];
     }
 }
