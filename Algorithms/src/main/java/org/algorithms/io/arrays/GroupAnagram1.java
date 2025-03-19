af8e05d314177719f82f438f07c6e0b781df8dc4
aqib javed <aqibbutt3078@gmail.com>
2025-03-19 01:02:03 +0000

    Leetcode 49 | Group Anagram

---
+++ Algorithms/src/main/java/org/algorithms/io/arrays/GroupAnagram1.java
@@ -0,0 +1,33 @@
+package org.algorithms.io.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagram1 {
+    // time complexity ON(logN) and space O(N)
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Base condition
+        if(strs == null || strs.length == 0)
+            return new ArrayList<>();
+
+        var groupAnagrams = new HashMap<String, List<String>>();
+        for(var str: strs) {
+            var key = getSortedKey(str);
+            if(groupAnagrams.containsKey(key)) {
+                groupAnagrams.get(key).add(str);
+            } else {
+                groupAnagrams.put(key, new ArrayList<>(List.of(str)));
+            }
+        }
+
+        return new ArrayList<>(groupAnagrams.values());
+    }
+
+    private String getSortedKey(String str) {
+        var data = str.toCharArray();
+        Arrays.sort(data);
+        return new String(data);
+    }
+}
