741dd0e17bfe74c160abe2cac7f70f341d15d01e
aqib javed <aqibbutt3078@gmail.com>
2025-03-24 22:14:01 +0000

    Graph DFS using stack

---
+++ Algorithms/src/main/java/org/algorithms/io/graphs/DepthFirstSearch.java
@@ -0,0 +1,56 @@
+package org.algorithms.io.graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class DepthFirstSearch {
+    public static void main(String[] args) {
+        var v1 = new Vertex("A");
+        var v2 = new Vertex("B");
+        var v3 = new Vertex("C");
+        var v4 = new Vertex("D");
+        var v5 = new Vertex("E");
+
+        List<Vertex> list = new ArrayList<>();
+        v1.addVertex(v2);
+        v1.addVertex(v3);
+        v3.addVertex(v4);
+        v4.addVertex(v5);
+
+        list.add(v1);
+        list.add(v2);
+        list.add(v3);
+        list.add(v4);
+        list.add(v5);
+
+        var dfs = new DepthFirstSearch();
+        dfs.dfs(list);
+    }
+    private final Stack<Vertex> lifo;
+    public DepthFirstSearch() {
+        this.lifo = new Stack<>();
+    }
+    public void dfs(List<Vertex> forest) {
+        for(Vertex f: forest) {
+            if(!f.isVisited())
+                dfs(f);
+        }
+    }
+
+    public void dfs(Vertex v) {
+        v.setVisited(true);
+        lifo.push(v);
+
+        while(!lifo.isEmpty()) {
+            var curr = lifo.pop();
+            System.out.println(curr);
+            for(var node: curr.getVertexes()) {
+                if(!node.isVisited()) {
+                    node.setVisited(true);
+                    lifo.push(node);
+                }
+            }
+        }
+    }
+}
