a079c58391eccf813c2bc26eef362255f277fbc6
aqib javed <aqibbutt3078@gmail.com>
2025-04-02 18:16:46 +0100

    Insertion Sort List

---
+++ Algorithms/src/main/java/org/algorithms/io/interviewPrep2k25/InsertionSortList.java
@@ -0,0 +1,37 @@
+package org.algorithms.io.interviewPrep2k25;
+
+import org.algorithms.io.linkedlist.ListNode;
+
+public class InsertionSortList {
+    public static void main(String[] args) {
+        var i = new InsertionSortList();
+        var head = new ListNode(9);
+        head.next = new ListNode(13);
+        head.next.next = new ListNode(4);
+        head.next.next.next = new ListNode(-7);
+        head.next.next.next.next = new ListNode(1);
+        head.next.next.next.next.next = new ListNode(2);
+        head.next.next.next.next.next.next = new ListNode(0);
+
+        System.out.println(i.sort(head));
+    }
+    public ListNode sort(ListNode head) {
+        if(head == null)
+            return null;
+        var dummy = new ListNode(Integer.MIN_VALUE);
+        var current = head;
+
+        while(current != null) {
+            var pre = dummy;
+            while(pre.next != null && pre.next.val < current.val) {
+                pre = pre.next;
+            }
+            var next = current.next;
+            current.next = pre.next;
+            pre.next = current;
+            current = next;
+        }
+
+        return dummy.next;
+    }
+}
