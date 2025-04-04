3a3508354765b12d3ac604a16c318f0a75596cfe
aqib javed <aqibbutt3078@gmail.com>
2025-04-04 01:02:03 +0100

    Bubble sort Recursive

--- Algorithms/src/main/java/org/algorithms/io/amazon2k25/sort/BubbleSortRecur.java
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k25/sort/BubbleSortRecur.java
@@ -1,5 +1,33 @@
 package org.algorithms.io.amazon2k25.sort;
 
+import java.util.Arrays;
+
 public class BubbleSortRecur {
 
+    public static void main(String[] args) {
+        var b = new BubbleSortRecur();
+        var data = new int[] {5, 6, 1, -1, 0, 22};
+        b.sort(data);
+        System.out.println(Arrays.toString(data));
+    }
+
+    public void sort(int []arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    public void sort(int[] arr, int start, int end) {
+        if(end < 0)
+            return;
+        if(start >= end)
+            return;
+
+        if(arr[start] > arr[start + 1]) {
+            int tmp = arr[start + 1];
+            arr[start + 1] = arr[start];
+            arr[start] = tmp;
+        }
+
+        sort(arr, start + 1, end);
+        sort(arr, 0, end - 1);
+    }
 }
