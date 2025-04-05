be0537386b7117197f849ab9c9bb6418b6073ffc
aqib javed <aqibbutt3078@gmail.com>
2025-04-05 01:02:03 +0100

    Bubble sort

---
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k25/sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package org.algorithms.io.amazon2k25.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        var sort = new BubbleSort();
+        var data = new int[] {5, 3, 1, 0, -5, 4};
+        sort.sort(data);
+        System.out.println(Arrays.toString(data));
+    }
+    public void sort(int[] arr) {
+        if(arr.length == 0)
+            return;
+        int limit = arr.length - 1;
+        while(limit >= 0) {
+            for(int i = 0; i < limit; i++) {
+                if(arr[i] > arr[i + 1]) {
+                    int tmp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+            limit--;
+        }
+    }
+}
---
+++ Algorithms/src/main/java/org/algorithms/io/amazon2k25/sort/BubbleSortRecur.java
@@ -0,0 +1,5 @@
+package org.algorithms.io.amazon2k25.sort;
+
+public class BubbleSortRecur {
+
+}
