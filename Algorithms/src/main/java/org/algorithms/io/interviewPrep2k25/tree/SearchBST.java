8c9e8f45fcb6bd387cb63139a0f017eb81f6eb2c
aqib javed <aqibbutt3078@gmail.com>
2025-04-14 01:02:03 +0100

    Search In BST

---
+++ Algorithms/src/main/java/org/algorithms/io/interviewPrep2k25/tree/SearchBST.java
@@ -0,0 +1,17 @@
+package org.algorithms.io.interviewPrep2k25.tree;
+
+import org.algorithms.io.tree.TreeNode;
+
+public class SearchBST {
+    // Time O(logN) and space O(logN)
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null || root.val == val)
+            return root;
+
+        if(val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+}
