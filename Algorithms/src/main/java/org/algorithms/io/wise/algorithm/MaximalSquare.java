c427847f2570e25290a7060da8c94b4d8dc9e679
aqib javed <aqibbutt3078@gmail.com>
2025-05-01 13:32:26 +0100

    Maximal squre using Dp

---
+++ Algorithms/src/main/java/org/algorithms/io/wise/algorithm/MaximalSquare.java
@@ -0,0 +1,42 @@
+package org.algorithms.io.wise.algorithm;
+
+public class MaximalSquare {
+    private int rows;
+    private int columns;
+    private int max;
+    private char[][] matrix;
+    private Integer[][] memo;
+    public int maximalSquare(char[][] matrix) {
+        this.rows = matrix.length;
+        this.columns = matrix[0].length;
+        this.memo = new Integer[rows][columns];
+        this.matrix = matrix;
+
+        helper(0, 0);
+
+        return max * max;
+    }
+
+    private int helper(int row, int column) {
+        if(row >= this.rows || column >= this.columns)
+            return 0;
+
+        if(memo[row][column] != null) {
+            return memo[row][column];
+        }
+
+        int right = helper(row, column + 1);
+        int diagonal = helper(row + 1, column + 1);
+        int down = helper(row + 1, column);
+        int min = Math.min(right, Math.min(diagonal, down));
+
+        int currentValue = matrix[row][column] == '1' ? 1 : 0;
+        if(currentValue == 1) {
+            currentValue += min;
+        }
+        memo[row][column] = currentValue;
+        max = Math.max(max, currentValue);
+
+        return currentValue;
+    }
+}
---
+++ Algorithms/src/main/java/org/algorithms/io/wise/algorithm/MaximalSquareDp.java
@@ -0,0 +1,28 @@
+package org.algorithms.io.wise.algorithm;
+
+public class MaximalSquareDp {
+
+    // Time complexity O(n + m) and space complexity O(n + m)
+    public int maximalSquare(char[][] matrix) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int[][] dp = new int[row + 1][column + 1];
+        int max = 0;
+
+        for(int i = 1; i < row + 1; i++) {
+            for(int j = 1; j < column + 1; j++) {
+                if(matrix[i - 1][j - 1] == '1') {
+                    int left = dp[i - 1][j];
+                    int diagonal = dp[i - 1][j - 1];
+                    int up = dp[i][j - 1];
+                    int min = Math.min(left, Math.min(diagonal, up));
+
+                    dp[i][j] = min + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+        return max * max;
+    }
+}
