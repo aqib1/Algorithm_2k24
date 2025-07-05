cfd3f9507d83d40eddfce948be2055ecb1a3aa49
aqib javed <aqibbutt3078@gmail.com>
2025-07-05 01:02:03 +0100

    Paran validator

--- Algorithms/src/main/java/org/algorithms/io/arrays/revision/ValidBrackets.java
+++ Algorithms/src/main/java/org/algorithms/io/arrays/revision/ValidBrackets.java
@@ -1,4 +1,34 @@
 package org.algorithms.io.arrays.revision;
 
-public class ValidParanthesis {
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidBrackets {
+    private static final Map<Character, Character> PARAM_PAIR = Map.of(
+            '}', '{',
+            ']', '[',
+            ')', '('
+    );
+
+    // Time complexity O(n) and space complexity O(n)
+    public boolean isValid(String s) {
+        var stack = new Stack<Character>();
+        var openingParam = new HashSet<>(PARAM_PAIR.values());
+        for (char ch : s.toCharArray()) {
+            if (PARAM_PAIR.containsKey(ch)) {
+                if (stack.isEmpty() || PARAM_PAIR.get(ch) != stack.pop()) {
+                    return false;
+                }
+            } else if (openingParam.contains(ch)) {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        var validParam = new ValidBrackets();
+        System.out.println(validParam.isValid("()[d({(a)c})]"));
+    }
 }
